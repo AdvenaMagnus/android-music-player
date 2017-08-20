51cda5ed9fac3b5ff513925ccfa8d2a8db21a974
BlueEyedDemon <alexfordeal@gmail.com>
2017-08-21 01:03:47 +0500

    Debug

--- app/src/main/java/com/example/alexander/musicplayer/CreatePlayListDialog.java
+++ app/src/main/java/com/example/alexander/musicplayer/CreatePlayListDialog.java
@@ -18,20 +18,22 @@ public class CreatePlayListDialog extends DialogFragment {
 
     Playlist playlist;
     View.OnClickListener goToNewPlaylist;
+    EditText playlistName;
 
     @Override
     public View onCreateView(final LayoutInflater inflater, final ViewGroup container,
                              Bundle savedInstanceState) {
         final LinearLayout ll = (LinearLayout) inflater.inflate(R.layout.create_playlist_dialog, container, false);
-        ll.findViewById(R.id.ok).setOnClickListener( getOkButtonListener(((EditText)ll.findViewById(R.id.playlist_title)).getText().toString()));
+        playlistName = ll.findViewById(R.id.playlist_title);
+        ll.findViewById(R.id.ok).setOnClickListener(getOkButtonListener());
         ll.findViewById(R.id.cancel).setOnClickListener(getCancelButtonListener());
         return ll;
     }
 
-    private View.OnClickListener getOkButtonListener(final String playlistName){
+    private View.OnClickListener getOkButtonListener(){
         return new View.OnClickListener() {
             public void onClick(View v) {
-                playlist.setName(playlistName);
+                playlist.setName(playlistName.getText().toString());
                 goToNewPlaylist.onClick(v);
                 CreatePlayListDialog.this.dismiss();
             }
